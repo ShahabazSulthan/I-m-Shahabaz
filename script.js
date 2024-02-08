1c96691fbd976e38148cb2e04d62015038f7d60e
ShahabazSulthan <158268765+ShahabazSulthan@users.noreply.github.com>
2024-02-08 14:08:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var tablinks = document.getElementsByClassName("tab-link");
+var tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+//-----------------menu-----------------------------
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu()
+{
+    sidemenu.style.right = "0";
+}
+
+function closemenu()
+{
+    sidemenu.style.right = "-200px";
+}
+
+// -------------Contact Form------------------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw94V3RwXx19T_vZc8iakcNVk9JmIjn6-bWuaHJ_AMxXqIFoIGGUqqrigzhTGJJhYg1/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg =document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML= "Message Sent Succesfully"
+      setTimeout(function(){
+        msg.innerHTML = "";
+      },1000)
+      form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
